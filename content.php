e686b64a28990114e685622592f2a17effa4d955
Lodoss <ahmednabeel1898@gmail.com>
2014-04-20 10:29:36 +0200

    Add condition to thumbnail

--- content.php
+++ content.php
@@ -33,28 +33,30 @@
 			        <param name=\"allowFullScreen\" value=\"true\" />
 			        <param name=\"allowscriptaccess\" value=\"always\" />
         			</object>");
-				$upload_dir = wp_upload_dir();
-				$image_data = file_get_contents($thumbnailyoutube);
-				$filename = basename($image_url);
-				if(wp_mkdir_p($upload_dir['path']))
-				    $file = $upload_dir['path'] . '/' . $filename;
-				else
-				    $file = $upload_dir['basedir'] . '/' . $filename;
-				file_put_contents($file, $image_data);
+				if ( !has_post_thumbnail()) {
+					$upload_dir = wp_upload_dir();
+					$image_data = file_get_contents($thumbnailyoutube);
+					$filename = basename($image_url);
+					if(wp_mkdir_p($upload_dir['path']))
+					    $file = $upload_dir['path'] . '/' . $filename;
+					else
+					    $file = $upload_dir['basedir'] . '/' . $filename;
+					file_put_contents($file, $image_data);
 
-				$wp_filetype = wp_check_filetype($filename, null );
-				$attachment = array(
-				    'post_mime_type' => $wp_filetype['type'],
-				    'post_title' => sanitize_file_name($filename),
-				    'post_content' => '',
-				    'post_status' => 'inherit'
-				);
-				$attach_id = wp_insert_attachment( $attachment, $file, $post_id );
-				require_once(ABSPATH . 'wp-admin/includes/image.php');
-				$attach_data = wp_generate_attachment_metadata( $attach_id, $file );
-				wp_update_attachment_metadata( $attach_id, $attach_data );
+					$wp_filetype = wp_check_filetype($filename, null );
+					$attachment = array(
+					    'post_mime_type' => $wp_filetype['type'],
+					    'post_title' => sanitize_file_name($filename),
+					    'post_content' => '',
+					    'post_status' => 'inherit'
+					);
+					$attach_id = wp_insert_attachment( $attachment, $file, $post_id );
+					require_once(ABSPATH . 'wp-admin/includes/image.php');
+					$attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+					wp_update_attachment_metadata( $attach_id, $attach_data );
 
-				set_post_thumbnail( $post_id, $attach_id );
+					set_post_thumbnail( $post_id, $attach_id );
+				}
 				the_title( '<h1 class="entry-title">', '</h1>' );
 				echo ("<img src=".$thumbnailyoutube">");
 				}else{
