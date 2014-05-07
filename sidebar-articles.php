f9e525679116cb7b6391d955f714133af0c082fa
programmar297 <amrhsn@gmail.com>
2014-05-07 23:25:37 +0900

    Update sidebar-articles.php

--- sidebar-articles.php
+++ sidebar-articles.php
@@ -27,4 +27,49 @@ if ( ! is_active_sidebar( 'sidebar-2' ) ) {
 			?>
 			</ul>
          	</aside>
+            <aside id="categories-3" class="widget widget_categories">
+				<h1 class="widget-title">Authors</h1>
+                <ul>
+                <?php 
+				 
+				$profid = $_REQUEST["pid"];
+				query_posts(array( 
+						'post_type' => 'books'
+					) );  
+				
+				$profileList =  array();
+				while (have_posts()) : the_post(); 
+						  $profile = get_post_meta($post->ID, '_wpcf_belongs_profile_id', true);
+						  if (!(empty( $profile ))) { 
+						  	$profileList[] = $profile;
+						  }
+						  
+						
+				endwhile;
+				wp_reset_postdata();
+				$argsauthors = array('post_type' => 'profile',  'post__in' => $profileList );
+				$the_query = new WP_Query( $argsauthors );
+					
+					
+					
+					
+				// The Loop
+				if ( $the_query->have_posts() ) {
+					  
+					while ( $the_query->have_posts() ) {
+						$the_query->the_post();
+						?>
+								<li class="cat-item"><a href="<?php the_permalink()?>" title=""><?php echo  get_the_title(). " (".get_post_meta( get_the_ID(), 'wpcf-profile-position', true ).")"; ?></a></li>
+					<?php
+					}
+				} else {
+					//echo ' no posts found';
+				}
+				/* Restore original Post Data */
+				wp_reset_postdata();
+			  
+			  
+			  ?></ul>
+
+            </aside>
 </div><!-- #articles-sidebar -->
