91b936cf648d93b971fe184aecdb911787b54cc8
programmar297 <amrhsn@gmail.com>
2014-05-17 00:08:11 +0900

    Update archive-library.php

--- archive-library.php
+++ archive-library.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The template for displaying BOOKS LIBRARY Archive pages
+ * The template for displaying Archive library pages
  *
- * Used to display archive-type pages if nothing more specific matches a query.
+ * Used to display article-type archive pages if nothing more specific matches a query.
  * For example, puts together date-based pages if no date.php file exists.
  *
  * If you'd like to further customize these archive views, you may create a
@@ -16,8 +16,8 @@
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
  */
-/*
-Template Name:Books Archive
+ /*
+Template Name:Library Archive
 */
 
 get_header(); ?>
@@ -51,6 +51,7 @@ get_header(); ?>
 					// Start the Loop.
 					$args = array( 'post_type' => 'library', 'posts_per_page' => 10 );
 					$loop = new WP_Query( $args );
+					//var_dump($loop);
 					while ( $loop->have_posts() ) : $loop->the_post();
 
 						/*
@@ -58,7 +59,7 @@ get_header(); ?>
 						 * use this in a child theme, then include a file called called content-___.php
 						 * (where ___ is the post format) and that will be used instead.
 						 */
-						get_template_part( 'content', get_post_format() );
+						get_template_part( 'content-library', get_post_format() );
 
 					endwhile;
 					// Previous/next page navigation.
@@ -66,7 +67,7 @@ get_header(); ?>
 
 				else :
 					// If no content, include the "No posts found" template.
-					get_template_part( 'content', 'none' );
+					get_template_part( 'content-library', 'none' );
 
 				endif;
 			?>
@@ -75,5 +76,6 @@ get_header(); ?>
 
 <?php
 //get_sidebar( 'content' );
+get_sidebar('content');
 get_sidebar();
 get_footer();
