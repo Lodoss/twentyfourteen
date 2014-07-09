999701526acac3cebfcbb5c886ad13a7157fd3cd
programmar297 <amrhsn@gmail.com>
2014-07-09 17:08:22 +0900

    Update single-library.php

--- single-library.php
+++ single-library.php
@@ -22,7 +22,7 @@ get_header(); ?>
 					 * use this in a child theme, then include a file called called content-___.php
 					 * (where ___ is the post format) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+					get_template_part( 'content-library', get_post_format() );
 
 					// Previous/next post navigation.
 					twentyfourteen_post_nav();
