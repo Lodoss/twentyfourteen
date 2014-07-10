14b3f8f692c7e636d77d699479bc3f650830f229
programmar297 <amrhsn@gmail.com>
2014-07-10 17:28:22 +0900

    Update content-articles.php

--- content-articles.php
+++ content-articles.php
@@ -61,7 +61,7 @@
 	<div class="entry-content">
 		<?php
 			//the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ) );
-			the_excerpt();
+			the_content();
 			wp_link_pages( array(
 				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
 				'after'       => '</div>',
