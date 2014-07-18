675ab35b40efee742f8464c44fbccebb4de48b3b
programmar297 <amrhsn@gmail.com>
2014-07-18 13:11:33 +0900

    Update sidebar-library.php

--- sidebar-library.php
+++ sidebar-library.php
@@ -78,6 +78,62 @@ if ( ! is_active_sidebar( 'sidebar-6' ) ) {
             </aside>
             <?php
 }
+else
+{ ?>
+	<aside id="categories-3" class="widget widget_categories">
+		<h1 class="widget-title">Books Categories</h1>
+        <?php
+				$profid = $_REQUEST["pid"];
+				query_posts(array( 
+						'post_type' => 'library'
+					) );  
+				
+				$profileList =  array();
+				while (have_posts()) : the_post(); 
+						  $profile = get_post_meta($post->ID, '_wpcf_belongs_profile_id', true);
+						  if (!(empty( $profile ))) { 
+						  	$profileList[] = $profile;
+						  }
+						  
+						
+				endwhile;
+				wp_reset_postdata();
+				$argsauthors = array('post_type' => 'profile',  'post__in' => $profileList );
+				$the_query = new WP_Query( $argsauthors );
+					
+					
+					
+					
+				// The Loop
+				if ( $the_query->have_posts() ) {
+					  
+					while ( $the_query->have_posts() ) {
+						$the_query->the_post();
+						?>
+								<h1 class="widget-title">More by <a href="<?php echo get_bloginfo('url') ?>/library-profile/?pid=<?php echo get_the_ID() ?>" title=""><?php echo  get_the_title()?></a></h1>
+					<?php
+					}
+				} else {
+					?>
+						<h1 class="widget-title">More from this author</a>
+                        <?
+				}
+				/* Restore original Post Data */
+				wp_reset_postdata();
+			?>
+            
+				
+                <ul>
+                <?php 
+				 
+				
+				
+			  
+			  
+			  ?></ul>
+
+            </aside> <?
+}
 ?>
 </div><!-- #library-sidebar -->
 
